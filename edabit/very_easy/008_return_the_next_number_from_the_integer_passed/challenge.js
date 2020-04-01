176ca530dbd66e58bb66b8e80aa83f963682ab00
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:37:06 +0200

    Update challenge.js

--- edabit/very_easy/008_return_the_next_number_from_the_integer_passed/challenge.js
+++ edabit/very_easy/008_return_the_next_number_from_the_integer_passed/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/NAQhEoxbofPidLxm9
+
+// Return the Next Number from the Integer Passed
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
 function addition(num) {
 	return num += 1;
 }
