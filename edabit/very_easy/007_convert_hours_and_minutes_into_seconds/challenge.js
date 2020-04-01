903c77005ba1dba30105d0b97261aec6c082164b
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:36:30 +0200

    Update challenge.js

--- edabit/very_easy/007_convert_hours_and_minutes_into_seconds/challenge.js
+++ edabit/very_easy/007_convert_hours_and_minutes_into_seconds/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/JesaFi5ntBEbGT8bu
+
+// Convert Hours and Minutes into Seconds
+// Write a function that takes two integers (hours, minutes) and converts them into seconds.
+
 function convert(hours, minutes) {
 	return (hours * 60 * 60) + (minutes * 60);
 }
