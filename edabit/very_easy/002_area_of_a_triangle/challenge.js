401db9f1510ce13beb502bb9c9d4fafebd55e5d5
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:31:13 +0200

    Update challenge.js

--- edabit/very_easy/002_area_of_a_triangle/challenge.js
+++ edabit/very_easy/002_area_of_a_triangle/challenge.js
@@ -1,3 +1,7 @@
+// https://edabit.com/challenge/3CaszbdZYGN4otQD8
+
+// Write a function that takes the base and height of a triangle and return its area.
+
 function triArea(base, height) {
 	return (base * height) / 2;
 }
