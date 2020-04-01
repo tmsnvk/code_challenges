2eef705167f6d99a6d90f6dbfeb58555786d7f1e
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:35:03 +0200

    Update challenge.js

--- edabit/very_easy/005_is_the_number_less_than_or_equal_to_zero/challenge.js
+++ edabit/very_easy/005_is_the_number_less_than_or_equal_to_zero/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/PTiLYyb4A69KZtBCg
+
+// Is the Number Less than or Equal to Zero?
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
 function lessThanOrEqualToZero(num) {
 	return num <= 0;
 }
