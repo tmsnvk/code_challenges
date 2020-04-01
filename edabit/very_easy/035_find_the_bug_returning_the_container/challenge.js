1826f6deff4acd3cd99eded82b2ececa5b705645
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 08:07:52 +0200

    Create challenge.js

---
+++ edabit/very_easy/035_find_the_bug_returning_the_container/challenge.js
@@ -0,0 +1,26 @@
+function getContainer(product) {
+	let container
+  switch (product) {
+		case "Bread":
+			container = "bag";
+			break;
+		case "Beer":
+			container = "bottle";
+			break;
+		case "Milk":
+			container = "bottle";
+			break;
+		case "Cerials":
+			container = "box";
+			break;
+		case "Eggs":
+			container = "carton";
+			break;
+		case "Candy":
+			container = "plastic";
+			break;
+		default:
+			container = null
+	}
+	return container
+}
