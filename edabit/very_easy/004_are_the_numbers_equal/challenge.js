4dee2dd0ad138a500612ba8154023a7a03562483
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:33:23 +0200

    Update challenge.js

--- edabit/very_easy/004_are_the_numbers_equal/challenge.js
+++ edabit/very_easy/004_are_the_numbers_equal/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/QSnaSH5S3oxZkwcNc
+
+// Are the Numbers Equal?
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
 function isSameNum(num1, num2) {
 	return num1 === num2;
 }
