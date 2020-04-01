82ec2b4fae5c63b361143f03193882d5e5b6cf5e
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-01 21:33:42 +0200

    Update challenge.js

--- edabit/very_easy/001_return_the_sum_of_two_numbers/challenge.js
+++ edabit/very_easy/001_return_the_sum_of_two_numbers/challenge.js
@@ -1,5 +1,6 @@
 // https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
 
+// Return the Sum of Two Numbers
 // Create a function that takes two numbers as arguments and return their sum.
 
 function addition(a, b) {
