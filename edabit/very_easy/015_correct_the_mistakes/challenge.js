ceda2f5bdfbb3a45015e654da9ac520e044497bf
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:07:25 +0200

    Update challenge.js

--- edabit/very_easy/015_correct_the_mistakes/challenge.js
+++ edabit/very_easy/015_correct_the_mistakes/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/ks3vMrqdnW3CQ3F4i
+
+// Correct the Mistakes
+// Fix the code in the code tab to pass this challenge (only syntax errors). 
+
 function squared(a) {
 	return a*a;
 }
