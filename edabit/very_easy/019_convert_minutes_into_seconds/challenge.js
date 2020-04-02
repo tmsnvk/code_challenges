e6e126eec871dbc3374c3e7e5d8c05514ee793a4
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:09:49 +0200

    Update challenge.js

--- edabit/very_easy/019_convert_minutes_into_seconds/challenge.js
+++ edabit/very_easy/019_convert_minutes_into_seconds/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+// Convert Minutes into Seconds
+// Write a function that takes an integer minutes and converts it to seconds.
+
 function convert(minutes) {
 	return minutes * 60;
 }
