d18d9b675869799482c74432a4085c103155cd01
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:12:34 +0200

    Update challenge.js

--- edabit/very_easy/031_divides_evenly/challenge.js
+++ edabit/very_easy/031_divides_evenly/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/JfB9mWmbwYHbupxCB
+
+// Divides Evenly
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+
 function dividesEvenly(a, b) {
 	return a % b === 0;
 }
