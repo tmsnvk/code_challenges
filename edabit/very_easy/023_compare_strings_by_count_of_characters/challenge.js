d9806b89c17d07978db90620991e4a418ab271a7
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:03:03 +0200

    Update challenge.js

--- edabit/very_easy/023_compare_strings_by_count_of_characters/challenge.js
+++ edabit/very_easy/023_compare_strings_by_count_of_characters/challenge.js
@@ -1,3 +1,9 @@
+// https://edabit.com/challenge/yHGowWucg3k2kJdZ4
+
+// Compare Strings by Count of Characters
+// Create a function that takes two strings as arguments and return either true or false depending on
+// whether the total number of characters in the first string is equal to the total number of characters in the second string.
+
 function comp(str1, str2) {
 	return str1.length === str2.length;
 }
