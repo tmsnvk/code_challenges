d7b731403e0a3a98cb7541b45bb2902d845d7670
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 18:56:18 +0200

    Create challenge.js

---
+++ edabit/very_easy/060_string_to_integer_and_vice_versa/challenge.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/EpCYjhzPKtCkkspL9
+
+// String to Integer and Vice Versa
+// Write two functions:
+
+// toInt() : A function to convert a string to an integer.
+// toStr() : A function to convert an integer to a string.
+
+function toInt(str) {
+	const x = parseInt(str);
+	return x;
+}
+
+function toStr(int) {
+	return int.toString();
+}
