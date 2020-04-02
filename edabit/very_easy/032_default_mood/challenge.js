ad622633be7c5c7f5ee532f478eec94e17ae4ed1
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:13:16 +0200

    Update challenge.js

--- edabit/very_easy/032_default_mood/challenge.js
+++ edabit/very_easy/032_default_mood/challenge.js
@@ -1,3 +1,9 @@
+// https://edabit.com/challenge/DdAkLaw4eyuWgCMmF
+
+// Default Mood
+// Create a function that takes in a current mood and return a sentence in the following format: 
+// "Today, I am feeling {mood}". However, if no argument is passed, return "Today, I am feeling neutral".
+
 function moodToday(mood) {
 	if (mood) {
 		return `Today, I am feeling ${mood}`;
