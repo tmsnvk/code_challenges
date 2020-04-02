14c0c5c63a9c6676707c25553269f75b53810fdb
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:14:40 +0200

    Update challenge.js

--- edabit/very_easy/034_hello_hello_world_world/challenge.js
+++ edabit/very_easy/034_hello_hello_world_world/challenge.js
@@ -1,3 +1,12 @@
+// https://edabit.com/challenge/uJqrcYkz4NvYi9eCY
+
+// Hello; Hello World; World
+// Write a function that takes an integer and:
+
+// If the number is a multiple of 3, return "Hello".
+// If the number is a multiple of 5, return "World".
+// If the number is a multiple of both 3 and 5, return "Hello World".
+
 function helloWorld(num) {
 	if (num % 3 === 0 && num % 5 === 0) {
 		return "Hello World";
