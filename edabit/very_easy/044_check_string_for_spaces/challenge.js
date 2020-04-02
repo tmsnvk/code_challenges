a3bc26e113fe5b7d9ca87118198ac38150be1551
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:41:34 +0200

    Update challenge.js

--- edabit/very_easy/044_check_string_for_spaces/challenge.js
+++ edabit/very_easy/044_check_string_for_spaces/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/RAoedjZwcGFhvRTru
+
+// Check String for Spaces
+// Create a function that returns true if a string contains any spaces.
+
 function hasSpaces(str) {
 	if (str.includes(" ")) {
 		return true;
