633056b80d0c6328711358ff6b9052747fd90234
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:00:27 +0200

    Update challenge.js

--- edabit/very_easy/011_less_than_100/challenge.js
+++ edabit/very_easy/011_less_than_100/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/9MjEpkL7yAjAqiH58
+
+// Less Than 100?
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
 function lessThan100(a, b) {
 	return (a + b) < 100;
 }
