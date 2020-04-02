9e09d811f1bb557de8a516e42c1672b2671df2fa
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:08:41 +0200

    Update challenge.js

--- edabit/very_easy/017_find_the_perimeter_of_a_rectangle/challenge.js
+++ edabit/very_easy/017_find_the_perimeter_of_a_rectangle/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/XnJ24rWW7iJkNrtsh
+
+// Find the Perimeter of a Rectangle
+// Create a function that takes height and width and finds the perimeter of a rectangle.
+
 function findPerimeter(height, width) {
 	return (height + width) * 2;
 }
