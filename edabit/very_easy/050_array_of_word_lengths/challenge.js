242997698448fc650d1aed762f6344b440e59555
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:46:00 +0200

    Update challenge.js

--- edabit/very_easy/050_array_of_word_lengths/challenge.js
+++ edabit/very_easy/050_array_of_word_lengths/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/B9yPEStmax7Jxb9Dj
+
+// Array of Word Lengths
+// Create a function that takes an array of words and transforms it into an array of each word's length.
+
 function wordLengths(arr) {
 	return arr.map(x => x.length);
 }
