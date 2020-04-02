f3508e027475aef9eadb8ec53332efe89db1f778
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:49:57 +0200

    Update challenge.js

--- edabit/very_easy/054_the_ph_scale/challenge.js
+++ edabit/very_easy/054_the_ph_scale/challenge.js
@@ -1,3 +1,9 @@
+// https://edabit.com/challenge/GmAuzN42QXxPNpSmF
+
+// The pH Scale
+// Given a pH value, return whether that value is 'alkaline', 'acidic', or 'neutral'. 
+// Return 'invalid' if the value given is less than 0 or greater than 14.
+
 function pHName(pH) {
 	if (pH < 7 && pH > 0) {
 		return "acidic";
