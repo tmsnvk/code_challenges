5a6a242d3421fda151a2a8a7938c20f7e8c3639c
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 21:55:57 +0200

    Create challenge.js

---
+++ edabit/very_easy/062_add_subtract_multiply_or_divide/challenge.js
@@ -0,0 +1,19 @@
+// https://edabit.com/challenge/XyPee4TdK64BTgvbD
+
+// Add, Subtract, Multiply or Divide?
+// Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24. 
+// If none of the operations can give 24, return null.
+
+function operation(num1, num2) {
+	if (num1 + num2 === 24) {
+		return "added";
+	} else if (num1 - num2 === 24) {
+		return "subtracted";
+	} else if (num1 * num2 === 24) {
+		return "multiplied";
+	} else if (num1 / num2 === 24) {
+		return "divided";
+	} else {
+		return null;
+	}
+}
