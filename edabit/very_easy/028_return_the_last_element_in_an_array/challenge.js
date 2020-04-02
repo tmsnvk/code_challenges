4061496b156db0f15d27882df14790441ab3b2c7
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:10:49 +0200

    Update challenge.js

--- edabit/very_easy/028_return_the_last_element_in_an_array/challenge.js
+++ edabit/very_easy/028_return_the_last_element_in_an_array/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/7JBTN4TbaxJQMdX9W
+
+// Return the Last Element in an Array
+// Create a function that accepts an array and returns the last item in the array.
+
 function getLastItem(arr) {
 	return arr.pop();
 }
