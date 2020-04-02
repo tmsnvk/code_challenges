519a9e7acf338c5dbeb6d62d857611905b34fa9f
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:26:07 +0200

    Update challenge.js

--- edabit/very_easy/035_find_the_bug_returning_the_container/challenge.js
+++ edabit/very_easy/035_find_the_bug_returning_the_container/challenge.js
@@ -1,3 +1,11 @@
+// https://edabit.com/challenge/kAfeaiu7QrXp4RfSX
+
+// Find the Bug: Returning the Container
+// The packaging system is running wild!
+// The candy is lying loose all over in the warehouse, the cereal is missing, and bread is stuffed in a bottle. 
+// What is going on here? The candy should be in plastic and the bread should be in a bag.
+// The packaging machine is running the getContainer() function to retrieve the container of a product. But something is not right...
+
 function getContainer(product) {
 	let container
   switch (product) {
@@ -22,5 +30,5 @@ function getContainer(product) {
 		default:
 			container = null
 	}
-	return container
+	return container;
 }
