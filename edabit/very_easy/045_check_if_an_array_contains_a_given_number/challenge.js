c533d354ee3710131e137c1872cc1d4436743c0a
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:42:09 +0200

    Update challenge.js

--- edabit/very_easy/045_check_if_an_array_contains_a_given_number/challenge.js
+++ edabit/very_easy/045_check_if_an_array_contains_a_given_number/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/SwyjHvkqwwQ2iJsoS
+
+// Check if an Array Contains a Given Number
+// Write a function to check if an array contains a particular number.
+
 function check(arr, el) {
 	return arr.includes(el);
 }
