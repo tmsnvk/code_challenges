ccb240c18532c9a525c67c9caa0a2e33f4b9a8fa
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 18:34:55 +0200

    Create challenge.js

---
+++ edabit/very_easy/057_case_insensitive_comparison/challenge.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/9MBwBaa3wDKX8Mxbv
+
+// Case Insensitive Comparison
+// Write a function that validates whether two strings are identical. Make it case insensitive.
+
+function match(s1, s2) {
+	return s1.toLowerCase() === s2.toLowerCase();
+}
