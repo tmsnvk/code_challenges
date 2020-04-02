ee0a21a6e5b268a32fe5bda2a7e8f3a6943b668a
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:11:59 +0200

    Update challenge.js

--- edabit/very_easy/030_name_greeting/challenge.js
+++ edabit/very_easy/030_name_greeting/challenge.js
@@ -1,1 +1,7 @@
+// https://edabit.com/challenge/6kdGMdd78jpZ45ujo
+
+// Name Greeting!
+// Create a function that takes a name and returns a greeting in the form of a string. 
+// Don't use a normal function but use an "arrow function".
+
 const helloName = name => `Hello ${name}!`;
