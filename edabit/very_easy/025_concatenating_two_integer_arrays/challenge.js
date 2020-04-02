09aaf9fd3964bcfa3405dfff4648cc10bb84ec7e
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:04:10 +0200

    Update challenge.js

--- edabit/very_easy/025_concatenating_two_integer_arrays/challenge.js
+++ edabit/very_easy/025_concatenating_two_integer_arrays/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/CzPEsTuXfE8J8vknX
+
+// Concatenating Two Integer Arrays
+// Create a function to concatenate two integer arrays.
+
 function concat(arr1, arr2) {
 	return arr1.concat(arr2);
 }
