35efaaa2fcc7bc57c348fa5ba28b1b3e476444d3
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:03:20 +0200

    Update challenge.js

--- edabit/very_easy/014_recursion_length_of_a_string/challenge.js
+++ edabit/very_easy/014_recursion_length_of_a_string/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/4MSbtYFBiRtxHEkY8
+
+// Recursion: Length of a String
+// Write a function that returns the length of a string. Make your function recursive.
+
 function length(str) {
 	return str.length;
 }
