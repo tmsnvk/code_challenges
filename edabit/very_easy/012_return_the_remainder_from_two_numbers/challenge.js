b5d03254f5f49f63cd617a5be410f197c4e06bec
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:01:24 +0200

    Update challenge.js

--- edabit/very_easy/012_return_the_remainder_from_two_numbers/challenge.js
+++ edabit/very_easy/012_return_the_remainder_from_two_numbers/challenge.js
@@ -1,3 +1,10 @@
+// https://edabit.com/challenge/Q2j5FTFtsk7PdzrQk
+
+// Return the Remainder from Two Numbers
+// There is a single operator in JavaScript, capable of providing the remainder of a division operation. 
+// Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. 
+// Return that value.
+
 function remainder(x, y) {
 	return x % y;
 }
