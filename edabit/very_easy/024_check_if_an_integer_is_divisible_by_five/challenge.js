762ebe8e6a93dfb6c60d9ebef4e48a74161eddca
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:03:36 +0200

    Update challenge.js

--- edabit/very_easy/024_check_if_an_integer_is_divisible_by_five/challenge.js
+++ edabit/very_easy/024_check_if_an_integer_is_divisible_by_five/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/iBQYbSHZGhpktLRdn
+
+// Check if an Integer is Divisible By Five
+// Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
 function divisibleByFive(n) {
 	return n % 5 === 0;
 }
