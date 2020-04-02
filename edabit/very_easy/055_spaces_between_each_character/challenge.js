6ee6b9c602969da64e4e304574d158aaae59a52e
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:50:34 +0200

    Update challenge.js

--- edabit/very_easy/055_spaces_between_each_character/challenge.js
+++ edabit/very_easy/055_spaces_between_each_character/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/8zbTPvexR9Aei3Csp
+
+// Spaces Between Each Character
+// Create a function that takes a string and returns a string with spaces in between all of the characters.
+
 function spaceMeOut(str) {
 	return str.split("").join(" ");
 }
