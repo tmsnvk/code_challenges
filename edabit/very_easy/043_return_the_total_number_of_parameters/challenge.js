9eff0bbd104a511b9425074b4863f236983c6345
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:40:52 +0200

    Update challenge.js

--- edabit/very_easy/043_return_the_total_number_of_parameters/challenge.js
+++ edabit/very_easy/043_return_the_total_number_of_parameters/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/hztqDz4mjCPpLMMF4
+
+// Return the Total Number of Parameters
+// Create a function that returns the total number of parameters passed in.
+
 function numberArgs(...num) {
 	return num.length;
 }
