6d0511ca047cc5cf778fadd2b8a80e1bb4dfbba9
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:11:25 +0200

    Update challenge.js

--- edabit/very_easy/029_concatenate_first_and_last_name_into_one_string/challenge.js
+++ edabit/very_easy/029_concatenate_first_and_last_name_into_one_string/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/RQwdZmtrW8mCnuCMN
+
+// Concatenate First and Last Name into One String
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
 function concatName(firstName, lastName) {
 	return `${lastName}, ${firstName}`;
 }
