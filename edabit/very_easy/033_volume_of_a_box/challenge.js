60fd6595608033756abe7e5e3376c3b6eee84779
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:13:49 +0200

    Update challenge.js

--- edabit/very_easy/033_volume_of_a_box/challenge.js
+++ edabit/very_easy/033_volume_of_a_box/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/ms3q5GYSpFpwxeFWX
+
+// Volume of a Box
+// Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+
 function volumeOfBox(sizes) {
 	return sizes.width * sizes.length * sizes.height;
 }
