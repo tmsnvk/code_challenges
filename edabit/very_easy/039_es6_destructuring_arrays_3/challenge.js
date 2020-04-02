b695883b75fe83f9e0281b65bca985764dcc5424
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:38:08 +0200

    Update challenge.js

--- edabit/very_easy/039_es6_destructuring_arrays_3/challenge.js
+++ edabit/very_easy/039_es6_destructuring_arrays_3/challenge.js
@@ -6,5 +6,5 @@
 // DO NOT USE var [lips] = arr[2]
 // "eyes", "nose", and "ears" should not be assigned to anything
 
-const arr = ["eyes", "nose", "lips", "ears"]
-var [,,lips,] = arr
+const arr = ["eyes", "nose", "lips", "ears"];
+var [,,lips,] = arr;
