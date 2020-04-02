ad04a3a064874b01d71644057b7bee5e7453425b
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:38:57 +0200

    Update challenge.js

--- edabit/very_easy/040_es6_destructuring_arrays_4/challenge.js
+++ edabit/very_easy/040_es6_destructuring_arrays_4/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/wJuXYhgetMtD4Gezj
+
+// ES6: Destructuring Arrays IV
+
 // Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4] 
 // Use the Rest element
-const str = '[head, ...tail] = [1, 2, 3, 4]'
+
+const str = "[head, ...tail] = [1, 2, 3, 4]";
