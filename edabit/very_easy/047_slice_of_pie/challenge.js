2e04ffc61503e240d9fdbb2e32f426ac5c119ae6
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:43:56 +0200

    Update challenge.js

--- edabit/very_easy/047_slice_of_pie/challenge.js
+++ edabit/very_easy/047_slice_of_pie/challenge.js
@@ -1,3 +1,15 @@
+// https://edabit.com/challenge/qkNvH9BZLTNtKQvae
+
+// Slice of Pie
+// Create a function that determines whether or not it's possible to split a pie fairly given these three parameters:
+
+// Total number of slices.
+// Number of recipients.
+// How many slices each person gets.
+
+// The function will be in this form:
+// `equalSlices(total slices, no. recipients, slices each)`
+
 function equalSlices(total, people, each) {
 	if (people * each <= total) {
 		return true;
