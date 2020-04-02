07774b73795bbd469b6b7ddd5f6e5ec4cd7a2d1b
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:10:06 +0200

    Update challenge.js

--- edabit/very_easy/027_find_the_index/challenge.js
+++ edabit/very_easy/027_find_the_index/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/9ApjPggCLGiPt573m
+
+// Find the Index
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
 function findIndex(arr, str) {
 	return arr.indexOf(str);
 }
