271df0fb632c75c27b2cc61a66348aff324b9934
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:27:36 +0200

    Update challenge.js

--- edabit/very_easy/037_es6_destructuring_arrays_1/challenge.js
+++ edabit/very_easy/037_es6_destructuring_arrays_1/challenge.js
@@ -1,3 +1,7 @@
-const arr = [1, 2, 3, 4, 5, 6]
+// https://edabit.com/challenge/sWAEoTbXA4bexBPb6
 
+// ES6: Destructuring Arrays I
+// Create variables a and b from the given array using the ES6 destructuring assignment syntax, where a === 1 and b === 2.
+
+const arr = [1, 2, 3, 4, 5, 6]
 const [a, b] = arr;
