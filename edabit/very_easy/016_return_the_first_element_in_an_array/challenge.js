683bd245793c379c98e47e64056e33699f4b5629
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:08:07 +0200

    Update challenge.js

--- edabit/very_easy/016_return_the_first_element_in_an_array/challenge.js
+++ edabit/very_easy/016_return_the_first_element_in_an_array/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/QaApgtePE6QrCZ64o
+
+// Return the First Element in an Array
+// Create a function that takes an array and returns the first element.
+
 function getFirstValue(arr) {
 	return arr.shift();
 }
