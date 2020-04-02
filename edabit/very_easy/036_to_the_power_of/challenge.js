9eb79b8d4348a0b724aaf12dbe938d9198b46282
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:26:38 +0200

    Update challenge.js

--- edabit/very_easy/036_to_the_power_of/challenge.js
+++ edabit/very_easy/036_to_the_power_of/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/H25aG5aAdmFcMpBsg
+
+// To the Power of _____
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
 function calculateExponent(num, exp) {
 	return Math.pow(num, exp);
 }
