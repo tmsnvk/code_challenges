8ba7605254ae6be85efe0f9d727bbccf161d762a
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 12:11:48 +0200

    Update challenge.js

--- edabit/very_easy/021_is_the_number_even_or_odd/challenge.js
+++ edabit/very_easy/021_is_the_number_even_or_odd/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/kuzB5CMXiKDEYKXAP
+
+// Is the Number Even or Odd?
+// Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+
 function isEvenOrOdd(num) {
 	if (num % 2 === 0) {
 		return "even";
