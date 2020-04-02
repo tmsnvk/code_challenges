8b9a95b82f09faa3ae79355f8a6119ed0da4d537
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:39:31 +0200

    Update challenge.js

--- edabit/very_easy/041_find_the_index_part_1/challenge.js
+++ edabit/very_easy/041_find_the_index_part_1/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/pEzxi4MqHGrAi7ZdA
+
+// Find the Index (Part 1)
+// Create a function that finds the index of a given item.
+
 function search(arr, item) {
 	return arr.indexOf(item);
 }
