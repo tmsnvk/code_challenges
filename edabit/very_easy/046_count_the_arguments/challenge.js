cd3002c6644d74f4155ba77aeb7c64ef58c0eb93
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:42:45 +0200

    Update challenge.js

--- edabit/very_easy/046_count_the_arguments/challenge.js
+++ edabit/very_easy/046_count_the_arguments/challenge.js
@@ -1,3 +1,8 @@
+// https://edabit.com/challenge/Mtke2HhuFwBkFi6Ty
+
+// Count the Arguments
+// Create a function that returns the number of argument it was called with.
+
 function numArgs(...rest) {
 	return rest.length;
 }
