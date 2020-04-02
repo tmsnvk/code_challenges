29672eb39567f1ac5b7c75765f54b73ce0fde05e
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 13:40:12 +0200

    Update challenge.js

--- edabit/very_easy/042_find_the_index_part_2/challenge.js
+++ edabit/very_easy/042_find_the_index_part_2/challenge.js
@@ -1,3 +1,9 @@
+// https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+
+// Find the Index (Part 2)
+// Create a function that searches for the index of a given item in an array. 
+// If the item is present, it should return the index, otherwise, it should return -1.
+
 function search(arr, item) {
 	return arr.indexOf(item);
 }
