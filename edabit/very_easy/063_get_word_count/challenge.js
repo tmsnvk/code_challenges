2138201ae63b3263ff4ac3ff2892a4c85ee4a2c9
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-02 21:59:15 +0200

    Create challenge.js

---
+++ edabit/very_easy/063_get_word_count/challenge.js
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/FydPqxRiosP7EBiQh
+
+// Get Word Count
+// Create a function that takes a string and returns the word count. The string will be a sentence.
+
+function countWords(str) {
+	const x = str.split(" ");
+	return x.length;
+}
