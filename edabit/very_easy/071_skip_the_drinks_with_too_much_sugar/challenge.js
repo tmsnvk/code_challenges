e547499e03dc71f435c1de3c7fe4f130184823c1
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-03 18:53:22 +0200

    Create challenge.js

---
+++ edabit/very_easy/071_skip_the_drinks_with_too_much_sugar/challenge.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/dgf76hmQ5Yw4FATFi
+
+// Skip the Drinks with Too Much Sugar
+// The function skipTooMuchSugarDrinks() takes in an array of drinks. 
+// Make sure the function only returns an array of drinks with no sugar in it or a little bit of sugar.
+// Drinks that contain too much sugar (in this challenge) are:
+
+// Cola
+// Fanta
+
+function skipTooMuchSugarDrinks(drinks) {
+	const x = drinks.filter(elem => elem !== "cola");
+	const y = x.filter(elem => elem !== "fanta");
+	
+  return y;
+}
