9ca864b00bbe502cb29ef73503fd333447123d95
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-03 19:41:22 +0200

    Create challenge.js

---
+++ codewars/8kyu/001_what_is_wrong_with_these_identifiers/challenge.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/56bb01de0e8b29de50000b19
+
+// An identifier is simply a name...
+// Can you amend this object so that its properties comprise only vaild identifiers?
+
+var Person = {
+  firstname: "John",
+  secondname: "Doe",
+  emailaddress: "john.doe@email.com",
+  male_female: "M"
+};
