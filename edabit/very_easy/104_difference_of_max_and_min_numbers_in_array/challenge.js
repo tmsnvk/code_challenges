ba240debc1acfa1eb18299b828a9c351736adfd2
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 16:42:45 +0200

    Create challenge.js

---
+++ edabit/very_easy/104_difference_of_max_and_min_numbers_in_array/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/v9DwaeR6NQoapEvHh
+
+// Difference of Max and Min Numbers in Array
+// Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+
+const diffMaxMin = arr => Math.max(...arr) - Math.min(...arr);
