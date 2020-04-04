37e0a2010d098a2e8b9628f906394ca124a4d22b
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 15:04:59 +0200

    Create challenge.js

---
+++ edabit/very_easy/089_find_the_smallest_number_in_an_array/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/g66EJsbdnSDayyEcK
+
+// Find the Smallest Number in an Array
+// Create a function that takes an array of numbers and returns the smallest number in the set.
+
+const findSmallestNum = arr => return Math.min(...arr);
