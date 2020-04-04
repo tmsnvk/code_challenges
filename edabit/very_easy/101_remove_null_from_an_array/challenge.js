cc903b2cdc1219e3cd5bf2c2858216521ef48164
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 16:25:44 +0200

    Create challenge.js

---
+++ edabit/very_easy/101_remove_null_from_an_array/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/2SJX9ha6W2bYkzSNX
+
+// Remove Null from an Array
+// Create a function to remove all null values from an array.
+
+const removeNull = arr => arr.filter(x => x !== null);
