3bd8e5d5af105660979aa03ce46b1f33602ba9c5
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 12:56:50 +0200

    Create challenge.js

---
+++ edabit/very_easy/084_find_the_largest_number_in_an_array/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/HTbRYCmPwfZGnBcxW
+
+// Find the Largest Number in an Array
+// Create a function that takes an array of numbers. Return the largest number in the array.
+
+const findLargestNum = arr => Math.max(...arr);
