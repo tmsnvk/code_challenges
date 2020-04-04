e09cd50e23e403385962015eee5985c1389b36d3
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 18:36:35 +0200

    Create challenge.js

---
+++ edabit/very_easy/115_reverse_the_order_of_a_string/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/oS3wCwLWsDqvs7XKd
+
+// Reverse the Order of a String
+// Create a function that takes a string as its argument and returns the string in reversed order.
+
+const reverse = str => str.split("").reverse().join("");
