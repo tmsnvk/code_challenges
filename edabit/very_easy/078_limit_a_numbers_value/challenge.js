e8e65cdb7496e8f5ee03600bc36868194876b958
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 12:17:08 +0200

    Create challenge.js

---
+++ edabit/very_easy/078_limit_a_numbers_value/challenge.js
@@ -0,0 +1,20 @@
+// https://edabit.com/challenge/ZwQJM6FsRsWd3Bm6g
+
+// Limit a Number's Value
+// Create a function that takes three number arguments. 
+// One number as an input and two additional numbers representing the endpoints of a closed range 
+// and return the number limited to this range.
+
+// If the number falls within the range, the number should be returned.
+// If the number is less than the lower limit of the range, the lower limit should be returned.
+// If the number is greater than the upper limit of the range, the upper limit should be returned.
+
+function limitNumber(num, rangeLow, rangeHigh) {
+	if (num > rangeLow && num < rangeHigh) {
+		return num;
+	} else if (num < rangeLow) {
+		return rangeLow;
+	} else {
+		return rangeHigh;
+	}
+}
