2a32f59c00f1d3403f15fa27c9d130122f9db08e
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 16:08:26 +0200

    Create challenge.js

---
+++ edabit/very_easy/099_return_the_four_letter_strings/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/havnWL4BimvoaXNMn
+
+// Return the Four Letter Strings
+// Create a function that takes an array of strings and returns the words that are exactly four letters.
+
+const isFourLetters = arr => arr.filter(x => x.length === 4);
