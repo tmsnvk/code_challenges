200bafc68390905cad17dd53db217abb3a062cc8
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 12:34:50 +0200

    Update challenge.js

--- edabit/very_easy/081_find_the_total_number_of_digits_the_given_number_has/challenge.js
+++ edabit/very_easy/081_find_the_total_number_of_digits_the_given_number_has/challenge.js
@@ -3,4 +3,4 @@
 // Find the Total Number of Digits the Given Number Has
 // Create a function that takes a number as an argument and returns the amount of digits it has.
 
-const findDigitAmount = num => num.toString().split("").length;
+const findDigitAmount = num => num.toString().length;
