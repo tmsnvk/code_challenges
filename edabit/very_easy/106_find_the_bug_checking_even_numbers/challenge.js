89942b29d26b88f998a44b68e10e589a67a2ac46
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 16:53:06 +0200

    Update challenge.js

--- edabit/very_easy/106_find_the_bug_checking_even_numbers/challenge.js
+++ edabit/very_easy/106_find_the_bug_checking_even_numbers/challenge.js
@@ -4,9 +4,7 @@
 // Create a function that takes in an array and returns true if all its values are even, and false otherwise.
 // Not a big deal, your friend says. He writes the following code:
 
-`function checkAllEven(arr) {
-  return arr.every(x % 2 === 0)
-}`
+// function checkAllEven(arr) { return arr.every(x % 2 === 0) }
 
 // The code above leads to a Reference Error, with x being undefined. Fix the code above so that all tests pass.
 
