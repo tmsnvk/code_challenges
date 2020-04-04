d7a34a7b37cf9fe891f6933ec94b6cb95728937c
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 15:22:26 +0200

    Create challenge.js

---
+++ edabit/very_easy/090_word_endings/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/hwWLHcDoPZmXcGHyd
+
+// Word Endings
+// Create a function that adds a string ending to each member in an array.
+
+const addEnding = (arr, ending) => arr.map(x => x + ending);
