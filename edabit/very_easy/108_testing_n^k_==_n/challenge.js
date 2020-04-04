ca6a30fabb9623a448db8cad7b49be4616990bdd
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-04 17:12:03 +0200

    Create challenge.js

---
+++ edabit/very_easy/108_testing_n^k_==_n/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/giAxQu4vjMRc7dJ2u
+
+// Testing K^K == N?
+// Write a function that returns true if k^k == n for input (n, k) and return false otherwise.
+
+const kToK = (n, k) => Math.pow(k, k) === n;
