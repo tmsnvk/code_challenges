1205a1be0925b81400d6af14230f1c364098e01f
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-05 15:56:31 +0200

    Delete 141challenge_retrieve_the_subreddit.js

--- edabit/very_easy/141challenge_retrieve_the_subreddit.js
+++
@@ -1,6 +0,0 @@
-// https://edabit.com/challenge/WT5MGmgaP3jvx8qpt
-
-// Retrieve the Subreddit
-// Create a function to extract the name of the subreddit from its URL.
-
-const subReddit = link => link.slice(25, link.length - 1);
