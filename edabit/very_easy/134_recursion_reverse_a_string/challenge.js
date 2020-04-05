a02ccfd41f4dacde0e66671b88d207d77f08bb0e
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-05 11:44:12 +0200

    Create challenge.js

---
+++ edabit/very_easy/134_recursion_reverse_a_string/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/TiAz6mfHTtkoqMszA
+
+// Recursion: Reverse a String
+// Write a function that reverses a string.
+
+const reverse = str => str.split("").reverse().join("");
