f7aba05b8a84f50d6e58ae85fc4babf573f5bf05
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-05 19:48:52 +0200

    Create challenge.js

---
+++ edabit/very_easy/147_a_circle_and_two_square/challenge.js
@@ -0,0 +1,10 @@
+// https://edabit.com/challenge/ApTfL3DXJtyAmiajv
+
+// A Circle and Two Square
+// Imagine a circle and two squares: a smaller and a bigger one.
+// For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+// Create a function, that takes an integer (radius of the circle) and returns the difference of the square-areas.
+
+function squareAreasDifference(r) {
+	return 2 * Math.pow(r, 2);
+}
