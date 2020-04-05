bcfeee6069d43b9bdb48c16e27585ed5f68f1d47
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-05 11:49:06 +0200

    Create challenge.js

---
+++ edabit/very_easy/135_edaaaaabit/challenge.js
@@ -0,0 +1,10 @@
+// https://edabit.com/challenge/tHZBvD3w69hpDCtMg
+
+// Edaaaaabit
+// Write a function that takes an integer and returns a string with the given number of "a"s in Edabit.
+
+function howManyTimes(num) {
+	const x = "a";	
+
+	return "Ed" + x.repeat(num) + "bit";
+}
