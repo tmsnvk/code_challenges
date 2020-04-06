2ae202fce46c9146e3f1e7f21205249b7af35adc
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-06 07:54:35 +0200

    Create challenge.js

---
+++ edabit/very_easy/156_fizzbuzz_interview_question/challenge.js
@@ -0,0 +1,21 @@
+// https://edabit.com/challenge/hoDbsz38GzWwBQ47w
+
+// FizzBuzz Interview Question
+// Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+
+// If the number is a multiple of 3 the output should be "Fizz".
+// If the number given is a multiple of 5, the output should be "Buzz".
+// If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+// If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+
+const FizzBuzz = num => {
+	if (num % 3 === 0 && num % 5 === 0) {
+		return "FizzBuzz";
+	} else if (num % 3 === 0) {
+		return "Fizz";
+	} else if (num % 5 === 0) {
+		return "Buzz";
+	} else {
+		return num.toString();
+	}
+}
