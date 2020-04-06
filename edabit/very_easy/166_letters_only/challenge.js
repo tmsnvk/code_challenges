00b7d869b1a26185dffa1b74f5bd531701644236
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-06 19:56:20 +0200

    Create challenge.js

---
+++ edabit/very_easy/166_letters_only/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/GPKkNbytRpsAnHcrX
+
+// Letters Only
+// Write a function that removes any non-letters from a string, returning a well-known film title.
+
+const lettersOnly = str => str.replace(/[^A-Za-z]/g, "");
