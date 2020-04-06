6a2fae0f3d25c7dce86a18f100e0b13b906fde1d
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-06 08:33:33 +0200

    Create challenge.js

---
+++ edabit/very_easy/161_extremely_over_nested/challenge.js
@@ -0,0 +1,5 @@
+// https://edabit.com/challenge/mptt94wPwNxqJJzgr
+
+// Create a function that returns the original value from a matrix with too many sub-arrays.
+
+const deNest = arr => arr.flat(Infinity)[0];
