a80e62dcbb93392a4e98d4ebed39588c59929bae
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-07 07:51:11 +0200

    Create challenge.js

---
+++ edabit/very_easy/176_get_the_file_extension/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/8A9E8LdE6zXcmPX3W
+
+// Get the File Extension
+// Write a function that maps files to their extension names.
+
+const getExtension = arr => arr.map(x => x.split(".")[1]);
