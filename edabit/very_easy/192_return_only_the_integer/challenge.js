f4f7239ee5a3f951dfecda436b133dca8a4021ba
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-07 21:39:44 +0200

    Create challenge.js

---
+++ edabit/very_easy/192_return_only_the_integer/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/3xqP79TrHiwnwGizj
+
+// Return Only the Integer
+// Write a function that takes an array of elements and returns only the integers.
+
+const returnOnlyInteger = arr => arr.filter(x => Number.isInteger(x));
