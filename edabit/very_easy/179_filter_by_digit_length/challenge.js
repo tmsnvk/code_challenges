40ce2a43bc8ebed31151a6c64c1cbf289128e9f2
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-07 08:27:24 +0200

    Create challenge.js

---
+++ edabit/very_easy/179_filter_by_digit_length/challenge.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/RXNybvGHLuvFiWKvq
+
+// Filter by Digit Length
+// Create a function that filters out an array to include numbers who only have a certain number of digits.
+// If no numbers of the specified digit length exist, return an empty array.
+// If all numbers in the array have the specified digit length, return original array.
+
+const filterDigitLength = (arr, num) => return arr.filter(x => x.toString().length === num);
