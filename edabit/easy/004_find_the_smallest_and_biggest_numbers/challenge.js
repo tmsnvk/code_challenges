a661570c4bf4edf25d049eaf85fb32caf769df25
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 14:41:20 +0200

    Create challenge.js

---
+++ edabit/easy/004_find_the_smallest_and_biggest_numbers/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/Q3n42rEWanZSTmsJm
+
+// Find the Smallest and Biggest Numbers
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+const minMax = arr => [Math.min(...arr), Math.max(...arr)];
