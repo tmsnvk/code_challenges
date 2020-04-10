7497da5c586f306b089967753a155808660e9b4a
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 09:20:02 +0200

    Create challenge.js

---
+++ edabit/very_easy/215_formatting_text_on_edabit/challenge.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/yxqTdf5a2PCbGKo9M
+
+// Formatting Text on Edabit
+
+function mdFormat(word, style) {
+	switch(style){
+		case "b":	return (`**${word}**`);
+			break;
+		case "i":	return (`_${word}_`);
+			break;
+		case "c":	return (`\`${word}\``);
+		break;
+		case "s":	return (`~~${word}~~`);
+		break;
+	}
+}
