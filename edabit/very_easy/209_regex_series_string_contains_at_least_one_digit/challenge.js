fa527811deeac5a5538e9b48e6a249875547843a
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 08:43:07 +0200

    Create challenge.js

---
+++ edabit/very_easy/209_regex_series_string_contains_at_least_one_digit/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/wBAuop24JYt9MZhXF
+
+// Regex Series: String Contains at Least One Digit
+// Write a regular expression that matches a string if it contains at least one digit.
+
+let x = /[0-9]+/;
