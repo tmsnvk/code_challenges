57d8d4b628d7992746686e8229788b97a4b2c7c5
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 14:24:55 +0200

    Create challenge.js

---
+++ edabit/easy/002_bitwise_operations/challenge.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/vvuAkYEAArrZvmp6X
+
+// Bitwise Operations
+// Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+const bitwiseAND = (n1, n2) => n1 & n2;
+const bitwiseOR = (n1, n2) => n1 | n2;
+const bitwiseXOR = (n1, n2) => n1 ^ n2;
