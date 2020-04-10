a6a9a89120a8be2647fb6bb5ef6d3d879eb6a40f
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 15:57:41 +0200

    Update challenge.js

--- edabit/easy/011_hiding_the_card_number/challenge.js
+++ edabit/easy/011_hiding_the_card_number/challenge.js
@@ -2,9 +2,6 @@
 
 // Hiding the Card Number
 // Write a function that takes a credit card number and only displays the last four characters. 
-The rest of the card number must be replaced by ************.
+// The rest of the card number must be replaced by ************.
 
-const cardHide = card => {
-	const x = card.length;
-	return `${"*".repeat(x - 4)}${card[x - 4]}${card[x - 3]}${card[x - 2]}${card[X - 1]}`;
-}
+const cardHide = card => `${"*".repeat(card.length - 4)}${card[card.length - 4]}${card[card.length - 3]}${card[card.length - 2]}${card[card.length - 1]}`;
