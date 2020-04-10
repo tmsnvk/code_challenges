9933953bc9dd068511ce040305c5928b83e8c5b8
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-10 09:05:15 +0200

    Create challenge.js

---
+++ edabit/very_easy/213_char_to_ascii/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/xPAPYC24EEpwzwQDy
+
+// Char-to-ASCII
+// Create a function that returns the ASCII value of the passed in character.
+
+const ctoa = c => c.charCodeAt(c);
