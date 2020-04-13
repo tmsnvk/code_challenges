2ac2b74e80c4b7c78ad7c5ec7e91698d267eaa08
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-13 21:42:38 +0200

    Create challenge.js

---
+++ edabit/easy/025_return_the_index_of_the_first_vowel/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/ppBDRwSoqZYcR95Aq
+
+// Return the Index of the First Vowel
+// Create a function that returns the index of the first vowel in a string.
+
+const firstVowel = str => str.toLowerCase().search(/[aeiou]/gi);
