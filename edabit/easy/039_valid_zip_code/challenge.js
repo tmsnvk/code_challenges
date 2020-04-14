0c5ad4f2ef381da84a3254b0853ee5fe1758bf90
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-14 21:04:42 +0200

    Create challenge.js

---
+++ edabit/easy/039_valid_zip_code/challenge.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/Ysk5M8XAscc4fqaAi
+
+// Valid Zip Code
+// Zip codes consist of 5 consecutive digits. 
+// Given a string, write a function to determine whether the input is a valid zip code. A valid zip code is as follows:
+
+// Must only contain numbers (no non-digits allowed).
+// Must not contain any spaces.
+// Must not be greater than 5 digits in length.
+
+const isValid = zip => zip.length === 5 && Number(zip);
