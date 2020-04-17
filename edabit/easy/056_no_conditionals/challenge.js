66979109f56314c8ece076bf351f7d45f706deaa
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-17 08:16:42 +0200

    Create challenge.js

---
+++ edabit/easy/056_no_conditionals/challenge.js
@@ -0,0 +1,7 @@
+// https://edabit.com/challenge/WjXHgXLAvMxNvD6h2
+
+// No Conditionals?
+// Write a function that returns 0 if the input is 1, and returns 1 if the input is 0.
+// Try completing this challenge without using any: Conditionals, Ternary operators, Negations, Bit operators.
+
+const flip = y => Math.abs(1-y);
