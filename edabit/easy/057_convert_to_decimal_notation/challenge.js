be503b1df85a491c1420716f0e41f321dba56d3a
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-17 08:20:08 +0200

    Create challenge.js

---
+++ edabit/easy/057_convert_to_decimal_notation/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/4tLabih2cr5Haw7xo
+
+// Convert to Decimal Notation
+// Create a function to convert an array of percentages to their decimal equivalents.
+
+const convertToDecimal = perc => perc.map(x => parseFloat(x) / 100);
