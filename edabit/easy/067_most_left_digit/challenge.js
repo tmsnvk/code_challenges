925ba0362670342a20d518af0c43b71684471c40
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-22 07:54:49 +0200

    Create challenge.js

---
+++ edabit/easy/067_most_left_digit/challenge.js
@@ -0,0 +1,6 @@
+// https://edabit.com/challenge/tPvx6pgPmerPrwqM8
+
+// Most Left Digit
+// Write a function that takes a string as an argument and returns the left most digit in the string.
+
+const leftDigit = num => Number(num.match(/[0-9]/));
