7b03015e90cac1f59989a1eb348e9f87fdbd196a
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-23 07:46:23 +0200

    Create challenge.js

---
+++ edabit/easy/073_regex_series_5_digit_zip_code/challenge.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/zmyuLfHcbJeTef5yr
+
+// Regex Series: 5-Digit Zip Code
+// Write a regular expression that matches a string if and only if it is a valid zip code.
+// Zipcodes must be 5 digits long exactly and only contain numbers.
+// Not allowed: non-numeric characters or whitespaces.
+
+let x = /^\d{5}$/;
